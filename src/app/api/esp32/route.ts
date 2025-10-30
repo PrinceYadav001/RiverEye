c59750c41f1201c8910f5dae4805cb881bc9216b
Prince Yadav <prince041670@gmail.com>
2025-10-30 09:55:50 +0530

    Initial commit for RiverEye project

--- src/app/api/esp32/route.ts
+++ src/app/api/esp32/route.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const res = await fetch("http://10.10.148.62/data");
+    const res = await fetch("http://10.10.216.164/data");
     const data = await res.json();
     return NextResponse.json(data, {
         headers: {
